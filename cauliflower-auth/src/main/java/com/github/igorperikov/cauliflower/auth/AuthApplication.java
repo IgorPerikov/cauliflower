63f9baf7117c3616a216af5dfb0171691dfc236b
IgorPerikov <perikov.igor@gmail.com>
2017-11-12 16:58:38 +0300

    init gradle scripts

---
+++ cauliflower-auth/src/main/java/com/github/igorperikov/cauliflower/auth/AuthApplication.java
@@ -0,0 +1,11 @@
+package com.github.igorperikov.cauliflower.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
---
+++ cauliflower-social/src/main/java/com/github/igorperikov/cauliflower/social/SocialApplication.java
@@ -0,0 +1,11 @@
+package com.github.igorperikov.cauliflower.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+}
