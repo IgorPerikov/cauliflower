911fa488c425b88559303e845c90e937c34bcab3
IgorPerikov <perikov.igor@gmail.com>
2017-11-25 07:33:23 +0300

    dockerize notes service

---
+++ cauliflower-notes/build.sh
@@ -0,0 +1,4 @@
+../gradlew clean build
+docker build -t igorperikov/cauliflower-notes:latest .
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push igorperikov/cauliflower-notes:latest
